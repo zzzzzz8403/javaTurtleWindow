0d5917cfae14489eb6c5a2029b2917767de426fc
ijfjfj <158243190+zzzzzz8403@users.noreply.github.com>
2024-10-04 14:40:05 -0400

    Create main.java

---
+++ main.java
@@ -0,0 +1,24 @@
+static void drawWindow(Turtle turtle)
+    {
+        faceRelative(turtle, 0, 1);
+        turtle.forward(50);
+        turtle.turnLeft();
+        turtle.forward(50);
+        turtle.turnLeft();
+        turtle.forward(50);
+        turtle.turnLeft();
+        turtle.forward(25);
+        turtle.turnLeft();
+        turtle.forward(50);
+        turtle.turnLeft();
+        turtle.forward(25);
+        turtle.turnLeft();
+        turtle.forward(25);
+        turtle.turnLeft();
+        turtle.forward(50);
+        turtle.backward(50);
+        turtle.turnRight();
+        turtle.forward(25);
+        turtle.turnLeft();
+        turtle.forward(50);
+    }
